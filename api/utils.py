15c70331d9bdf9faa74edb99e3394c870061561e
Muhammad Elkfrawy <mohamedelkfrawy@paymob.com>
2024-08-12 16:17:55 +0300

    chore: add server name

--- api/utils.py
+++ api/utils.py
@@ -1,8 +1,9 @@
 from django.utils import timezone
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from django.conf import settings
 
 
 @api_view(["GET"])
 def health_check(request):
-    return Response({"status ": "ok", "server_time": timezone.localtime()})
+    return Response({"status ": "ok", "server_time": timezone.localtime(), "server_name": settings.SERVER_NAME})
