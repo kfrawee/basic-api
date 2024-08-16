ed70157cf245a1d2afd4a990bd71e62048becad4
Muhammad Elkfrawy <mohamedelkfrawy@paymob.com>
2024-08-16 20:35:31 +0300

    fix: load .env

--- base/settings.py
+++ base/settings.py
@@ -24,6 +24,8 @@ env = environ.Env(
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Read .env (Some times it not loaded automatically)
+environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
